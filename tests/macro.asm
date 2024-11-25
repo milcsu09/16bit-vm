4ac98fea9dbcd0802e7c5c6b37ee2d4af096cd16
milcsu09 <petomilan2009@gmail.com>
2024-11-25 20:51:49 +0100

    ASSEMBLER: basic macros

---
+++ tests/macro.asm
@@ -0,0 +1,17 @@
+
+%define stack-frame-create
+  push %bp
+  mov %bp %sp
+%end
+
+%define stack-frame-destroy
+  mov %sp %bp
+  pop %bp
+%end
+
+.function
+  %stack-frame-create
+
+  %stack-frame-destroy
+  ret
+
