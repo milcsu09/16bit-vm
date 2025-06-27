0a8372b8d187c017add3b0ebb2f972ff92af8c47
milcsu09 <petomilan2009@gmail.com>
2025-06-27 20:09:11 +0200

    Basic TTY frontend

--- frontend/tty.c
+++ frontend/tty.c
@@ -1,6 +1,28 @@
+#include "../vm/vm.h"
+
+#include <stdio.h>
 
 int
-main ()
+main (int argc, char **argv)
 {
+  if (argc <= 1)
+    {
+      fprintf (stderr, "USAGE: %s <ROM>\n", argv[0]);
+      return 1;
+    }
+
+  VM vm = {0};
+
+  vm_create (&vm);
+
+  if (!vm_load_file (&vm, argv[1]))
+    return 1;
+
+  while (!vm.halt)
+    {
+      vm_step (&vm);
+    }
+
+  vm_destroy (&vm);
 }
 
