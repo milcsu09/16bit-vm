cc6b67cdc135ced5a4f81d65bcd75292ad1b2dc7
milcsu09 <petomilan2009@gmail.com>
2025-06-27 20:55:53 +0200

    Example of writer device

--- examples/tty_writer.asm
+++ examples/tty_writer.asm
@@ -1,5 +1,5 @@
 
-WRITE_BUFFER_ADDRESS = 0x3000
+WRITER_BYTE_ADDRESS = 0x3000
 
 mov w r5 message
 call write
@@ -11,21 +11,19 @@ message: def "Hello, world!\n\0"
 write:
   pusha
 
-  mov w r1 WRITE_BUFFER_ADDRESS
-
 write_loop:
   mov r2 (r5)
 
   cmp r2 '\0
   jeq write_end
 
-  mov (r1) r2
+  mov (WRITER_BYTE_ADDRESS) r2
 
-  add r1 r1 1
   add r5 r5 1
   jmp write_loop
 
 write_end:
   popa
   ret
+
 
--- frontend/tty.c
+++ frontend/tty.c
@@ -2,6 +2,13 @@
 
 #include <stdio.h>
 
+void
+writer_store_byte (VM *vm, VM_Device *device, word address, byte value)
+{
+  (void)vm, (void)device, (void)address;
+  putc (value, stdout);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -14,6 +21,16 @@ main (int argc, char **argv)
   VM vm = {0};
 
   vm_create (&vm);
+
+  VM_Device writer = {0};
+
+  writer.read_byte = vm_default_read_byte;
+  writer.read_word = vm_default_read_word;
+  writer.store_byte = writer_store_byte;
+  writer.store_word = vm_default_store_word;
+  writer.state = NULL;
+
+  vm_map_device (&vm, &writer, 0x3000, 0x3100);
 
   if (!vm_load_file (&vm, argv[1]))
     return 1;
