e9c54f9ec41a91267a35158e6d6fbe580ca911b1
milcsu09 <petomilan2009@gmail.com>
2025-07-05 19:35:51 +0200

    Test

--- examples/02_sdl_render_char.asm
+++ examples/02_sdl_render_char.asm
@@ -7,13 +7,13 @@ loop:
 
   mov r5 10
   mov r6 10
-  mov r7 0xF77
+  mov r7 0x77F
   mov r8 s1
   call sdl_render_str
 
   mov r5 10
-  mov r6 40
-  mov r7 0x7F7
+  mov r6 18
+  mov r7 0xF77
   mov r8 s2
   call sdl_render_str
 
@@ -23,5 +23,5 @@ loop:
 halt
 
 s1: defb "Hello, world!" 0
-s2: defb "2 + 2 = 2 ^ 2" 0
+s2: defb "^~~~~  ^~~~~" 0
 
